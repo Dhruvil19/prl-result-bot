bbdc37e14f0e487b02e01f90679232203e6d1858
Dhruvil19 <103842822+Dhruvil19@users.noreply.github.com>
2026-01-31 11:54:31 +0530

    Refactor PRL bot to use secrets and add timeout

--- check_prl.py
+++ check_prl.py
@@ -1,30 +1,54 @@
+# import requests
+
+# URL = "https://www.prl.res.in/prl-eng/advertisement"
+# KEYWORDS = ["result", "written test result", "selection","Technical Assistance"]
+
+# BOT_TOKEN = "8410171083:AAH6ivYb2vE3nazTIjOihNuDIaJdGZIAPPc"
+# CHAT_ID = "2043365711"
+# # telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+# # requests.post(
+# #     telegram_url,
+# #     data={
+# #         "chat_id": CHAT_ID,
+# #         "text": "✅ TEST MESSAGE: PRL BOT is working!"
+# #     }
+# # )
+
+# # print("Test message sent")
+
+# headers = {"User-Agent": "Mozilla/5.0"}
+
+# page = requests.get(URL, headers=headers).text.lower()
+
+# for word in KEYWORDS:
+#     if word in page:
+#         telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+#         requests.post(telegram_url, data={
+#             "chat_id": CHAT_ID,
+#             "text": "🚨 PRL RESULT UPDATE FOUND!\nhttps://www.prl.res.in/prl-eng/advertisement"
+#         })
+#         break
 import requests
 
 URL = "https://www.prl.res.in/prl-eng/advertisement"
-KEYWORDS = ["result", "written test result", "selection","Technical Assistance"]
+KEYWORDS = ["result", "written test result", "selection"]
 
-BOT_TOKEN = "8410171083:AAH6ivYb2vE3nazTIjOihNuDIaJdGZIAPPc"
-CHAT_ID = "2043365711"
-# telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-# requests.post(
-#     telegram_url,
-#     data={
-#         "chat_id": CHAT_ID,
-#         "text": "✅ TEST MESSAGE: PRL BOT is working!"
-#     }
-# )
-
-# print("Test message sent")
+BOT_TOKEN = "${{ secrets.BOT_TOKEN }}"
+CHAT_ID = "${{ secrets.CHAT_ID }}"
 
 headers = {"User-Agent": "Mozilla/5.0"}
 
-page = requests.get(URL, headers=headers).text.lower()
+page = requests.get(URL, headers=headers, timeout=20).text.lower()
 
 for word in KEYWORDS:
     if word in page:
         telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        requests.post(telegram_url, data={
-            "chat_id": CHAT_ID,
-            "text": "🚨 PRL RESULT UPDATE FOUND!\nhttps://www.prl.res.in/prl-eng/advertisement"
-        })
+        requests.post(
+            telegram_url,
+            data={
+                "chat_id": CHAT_ID,
+                "text": "🚨 PRL RESULT UPDATE FOUND!\nhttps://www.prl.res.in/prl-eng/advertisement"
+            },
+            timeout=20
+        )
         break
